cfb686926b9e961bdc58f4ae01a2782e0e79f94b
shreyansh-35 <2021pgcaca035@gmail.com>
2024-02-01 13:43:37 +0530

    Link Change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
             <span className='text-[#eb7f05] font-serif text-3xl font-semibold px-5' >Summarizing Tool</span>
         </Link>
 
-        <Link to='https://github.com/sakshitripathi28' target='_blank'>
+        <Link to='https://github.com/shreyansh-35' target='_blank'>
             <button className='rounded-full bg-black text-[white] text-base tracking-wide px-6 py-2'> GITHUB</button>
         </Link>
         
